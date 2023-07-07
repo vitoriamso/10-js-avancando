721ae2ec92be3727935ec4ebd3a7bfa3054d9ace
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 22:28:20 +0000

    melhorias

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function finalizar(){
         }
         media = soma/total
         res.innerHTML = ''
-        res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> Ao todo temos ${total} números cadastrados.</p>`
         res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
         res.innerHTML += `<p> A soma de todos os valores é ${soma}.</p>`
