7ca62cd360009045dbb2d261e08321461b40cd3f
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 21:51:27 +0000

    modificando o estilo css

--- script.js
+++ script.js
@@ -48,26 +48,26 @@ function finalizar(){
         window.alert("Adicione valores antes de finalizar!")
     }
     else{
-        let tot = valores.length;
-        let maior = valores[0];
-        let menor = valores[0];
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
         let soma = 0
         let media = 0
         for(let pos in valores){
-            soma += valores[pos];
+            soma += valores[pos]
             if(valores[pos] > maior){
-                maior = valores[pos];
+                maior = valores[pos]
             }
             if(valores[pos] < menor){
-                menor = valores[pos];
+                menor = valores[pos]
             }
         }
         media = soma / tot;
         res.innerHTML = "";
-        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`;
-        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`;
-        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`;
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`;
-        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`;
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
     }
 }
