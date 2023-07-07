27d8954568f1bd65f01ec264fbfd660947080cef
Maria Vitória <133173885+vitoriamso@users.noreply.github.com>
2023-07-07 22:26:10 +0000

    consertando erros

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
-
-let num = document.querySelector('input#fnum')
-let lista = document.querySelector("select#flista")
-let res = document.querySelector("div#res")
+let num = document.getElementById('fnum')
+let lista  =  document.getElementById('flista')
+let res = document.getElementById('res')
 let valores = []
 
 function isNumero(n){
-    if(Number(n) >= 1 && Number(n) <= 100){
+    if(Number(n) >= 1 && Number(n) <=100){
         return true
-    }
-    else{
+    }else{
         return false
     }
 }
-function inLista(n,l){
-    if(l.indexOf(Number(n)) != -1){
-        return false;
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
     }
-    else{
-        return true;
-    }
+
 }
 function adicionar(){
-    if(isNumero(num.value) && inLista(num.value, valores)){
+    if(isNumero(num.value) && ! inlista(num.value, valores)){
         valores.push(Number(num.value))
-        let item = document.createElement("option")
-        item.text = `Valor ${num.value} adicionado .`
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
         res.innerHTML = ''
-    }
-    else{
-        window.alert("Valor inválido ou ja encontrado na lista")
+    }else{
+        alert('Valor inválido ou já inserido na lista')
     }
     num.value = ''
     num.focus()
+
 }
+
 function finalizar(){
     if(valores.length == 0){
-        window.alert("Adicione valores antes de finalizar!")
-    }
-    else{
+        alert('adicione valores antes de finalizar!')
+    }else{
         let total = valores.length
         let maior = valores[0]
         let menor = valores[0]
         let soma = 0
         let media = 0
-        for(let pos in valores){
+
+        for (let pos in valores) {
             soma += valores[pos]
-            if(valores[pos] > maior){
+
+            if (valores[pos] > maior)
                 maior = valores[pos]
-            }
-            if(valores[pos] < menor){
+            
+            if (valores[pos] < menor)
                 menor = valores[pos]
-            }
         }
-        media = soma / total;
-        res.innerHTML = "";
-        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
-        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
-        res.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+        media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p> A soma de todos os valores é ${soma}.</p>`
+        res.innerHTML += `<p> A média de todos os valores é ${media}.</p>`
     }
 }
